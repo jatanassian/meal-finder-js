cce197ba05740620c1093252904f02d1a63007f5
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-24 14:37:29 -0400

    fetch the meal with its ID and display the ingredients and instructions to the DOM

--- script.js
+++ script.js
@@ -41,7 +41,49 @@ function searchMeal(e) {
   } else {
     alert('Please enter a search term');
   }
-  
+}
+
+// Fetch meal by ID
+function getMealById(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then(res => res.json)
+    .then(data => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
+// Add meal to DOM
+function addMealToDOM(meal) {
+  const ingredients = [];
+
+  // Create an array of ingredient and quantity from the data of the API
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`stringIngredient${i}`]) {
+      ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+    } else {
+      break;
+    }
+  }
+
+  single_mealEl.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map(ing => `<li>${ing}</li>`).joing('')}
+        </ul>
+      </div>
+    </div>
+  `;
 }
 
 
