b3e0e449e2b119c3a439bfc27d198008f56682df
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-24 10:43:14 -0400

    create searchMeal function that fetches from the API and output html to the DOM

---
+++ script.js
@@ -0,0 +1,34 @@
+const search = document.getElementById('search'),
+  submit = document.getElementById('submit'),
+  random = document.getElementById('random'),
+  mealsEl = document.getElementById('meals'),
+  resultHeading = document.getElementById('result-heading'),
+  single_mealEl = document.getElementById('single-meal');
+
+// Seach meal and fetch from API
+function searchMeal(e) {
+  e.preventDefault();
+
+  // Clear single meal
+  single_mealEl.innerHTML = '';
+
+  // Get search term
+  const term = search.nodeValue;
+
+  // Check for empty
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+  } else {
+    alert('Please enter a search term');
+  }
+
+}
+
+
+
+// Event listeners
+submit.addEventListener('submit', searchMeal);
